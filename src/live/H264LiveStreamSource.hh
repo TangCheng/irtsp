a7fbac274b07298332725163ddcc963d6f695885
唐成 <tangcheng2005@gmail.com>
2014-07-30 11:16:05 +0800

    将C++版本的zmq头文件直接包含到工程中。

--- src/live/H264LiveStreamSource.hh
+++ src/live/H264LiveStreamSource.hh
@@ -28,7 +28,7 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #include <FramedSource.hh>
 #endif
 
-#include <zmq.hpp>
+#include "cppzmq/zmq.hpp"
 
 // The following class can be used to define specific encoder parameters
 /*
